2863cf1c999aa61960e220b5cb61602ea99f6f14
vodarerp <51379937+vodarerp@users.noreply.github.com>
2025-09-09 16:55:59 +0200

    added Migration.Infrastructure

---
+++ Migration.Infrastructure/Implementation/FolderScanner.cs
@@ -0,0 +1,60 @@
+﻿using Alfresco.Apstraction.Interfaces;
+using Alfresco.Contracts.Request;
+using Migration.Apstaction.Interfaces;
+using Migration.Apstaction.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migration.Infrastructure.Implementation
+{
+    public class FolderScanner : IFolderScanner
+    {
+        private readonly IAlfrescoReadApi _read;
+
+
+        public FolderScanner(IAlfrescoReadApi read)
+        {
+             _read = read;
+        }
+        public async Task<IReadOnlyList<FolderSource>> ScanAsync(ScanRequest inRequest, CancellationToken ct)
+        {
+            var cmsLike = string.IsNullOrWhiteSpace(inRequest.nameFilter) ? "" : inRequest.nameFilter;
+            var req = new PostSearchRequest()
+            {
+                Query = new QueryRequest()
+                {
+                    Language = "cmis",
+                    Query = $"SELECT * FROM cmis:folder WHERE cmis:name LIKE '%{cmsLike}%' order by cmis:name"
+                },
+                Paging = new PagingRequest()
+                {
+                    MaxItems = inRequest.take,
+                    SkipCount = inRequest.skip
+                },
+                Sort = null
+            };
+
+            var resp = await _read.SearchAsync(req, ct);
+
+            var result = new List<FolderSource>(resp?.List?.Entries?.Count ?? 0);
+
+            if (resp?.List?.Entries != null)
+            {
+                foreach (var item in resp.List.Entries)
+                {
+                    if (item?.Entry != null)
+                    {
+                        result.Add(new FolderSource(item.Entry.Id, item.Entry.Name, item.Entry.ParentId ?? inRequest.rootId, ""));
+                    }
+                }
+            }
+
+
+            return result;
+        }
+    }
+}
